9a791287441c4fe163c67ed0720fcaf60cc62bea
tristan-heilmann <heilmanntristan2@gmail.com>
2024-06-13 14:53:09 -0400

    guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
+
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random_generator = new Random();
+        int magic_number = random_generator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magic_number)
+
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic_number)
+
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magic_number)
+
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
