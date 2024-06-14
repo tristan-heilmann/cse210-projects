69c2170b5211ec866483db08e91847bf4f4dcaa2
tristan-heilmann <heilmanntristan2@gmail.com>
2024-06-13 23:37:11 -0400

    lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int user_number = -1;
+        while (user_number != 0)
+        
+        {
+            Console.Write("Enter a number. Type 0 when finished. ");
+
+            string user_response = Console.ReadLine();
+            user_number = int.Parse(user_response);
+
+            if (user_number != 0)
+
+            {
+                numbers.Add(user_number);
+            }
+        }
+
+         int sum = 0;
+
+        foreach (int number in numbers)
+
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+
+        {
+            if (number > max)
+
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
