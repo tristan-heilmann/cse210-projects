5bd2a915bd038cffc3288be16d69eb63f8d0229a
tristan-heilmann <heilmanntristan2@gmail.com>
2024-06-18 17:46:33 -0400

    Work application

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,18 +7,25 @@ class Program
         Console.WriteLine("Hello Learning02 World!");
 
         Job job1 = new Job();
+
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
         Job job2 = new Job();
+
         job2._jobTitle = "Manager";
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
-        Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
+        Resume myResume = new Resume();
+
+        myResume._name = "Tristan Heilmann";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
     }
 }
