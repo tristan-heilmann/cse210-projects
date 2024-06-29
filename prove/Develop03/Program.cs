3d8869b422a41405464affa2d2c45deef7894d4b
tristan-heilmann <heilmanntristan2@gmail.com>
2024-06-29 18:57:28 -0400

    Articulation

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine();
+
+        Console.WriteLine("Press enter or type 'Quit' to finish.");
+        Console.ReadLine();
+
+        Console.Clear();
+
+        Console.WriteLine("Boo");
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,24 @@
+public class Scripture
+
+{
+    private Reference _reference = new Reference();
+    private List<Word> _words = new List<Word>();
+
+    public void HideRandomWords()
+
+    {
+        
+    }
+
+    public string GetDisplayText()
+
+    {
+        
+    }
+
+    public bool IsCompletelyHidden()
+
+    {
+
+    }
+}
