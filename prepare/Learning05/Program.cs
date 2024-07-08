9489af2f70799a0b25ce20fdfcb33b1d3df8e89d
tristan-heilmann <heilmanntristan2@gmail.com>
2024-07-08 14:58:35 -0400

    polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 1);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("yellow", 2, 3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("blue", 4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
